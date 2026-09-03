dfba85957b5b38425b2c570b126dde20fac13dca
pratikfortune4 <pratik.y@fortune4.in>
2026-09-03 10:02:50 +0530

    refactor: remove obsolete database cleanup and migration drop utility scripts

--- drop_schema.ts
+++
@@ -1,5 +0,0 @@
-﻿import postgres from 'postgres';
-const sql = postgres(process.env.DATABASE_URL);
-sql\DROP SCHEMA public CASCADE; CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO postgres; GRANT ALL ON SCHEMA public TO public;\
-  .then(() => { console.log('Schema dropped'); process.exit(0); })
-  .catch((e) => { console.error(e); process.exit(1); });
